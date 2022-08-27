6a6fc11aef686172fd4ad4b522a989851771c38e
antonellabogado <antonellabogadob@gmail.com>
2022-08-27 19:33:59 -0300

    operadores avanzados2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
-// DEFINICIÓN DE CONST
 // Modal
 const modal = document.querySelector('.modal');
-const abrirModal = document.querySelector('.btn-agregar-tarea');
-const cerrarModal = document.querySelector('.cerrar-modal');
+// bienvenida
+const modalBienvenida = document.querySelector('#bienvenida');
+// agregar tarea
+const modalAggTarea = document.querySelector('#agregar-tarea');
+const abrirAggTarea = document.querySelector('.btn-agregar-tarea');
+const cerrarAggTarea = document.querySelector('.cerrar-agg-tarea');
+// agregar categoria
+const modalAggCategoria = document.querySelector('#agregar-categoria');
+const abrirAggCategoria = document.querySelector('.btn-agg-categoria');
+const cerrarAggCategoria = document.querySelector('.cerrar-agg-categoria');
 
 // Card 
 const cardContainer = document.querySelector('.card-container');
 
-// Formulario
+// Formularios
+const formBienvenida = document.querySelector('#form-bienvenida');
 const formAggTarea = document.querySelector('#form-agregar-tarea');
 
 // Saludo
 const saludo = document.querySelector('.saludo');
+let usuario = localStorage.getItem('user') || '';
+usuario === ''? modalBienvenida.classList.add('modal--show') : saludar(usuario);
 
 // Lista de tareas
-let tareas=[];
-if(localStorage.getItem('tareas')!=null){
-    tareas=JSON.parse(localStorage.getItem('tareas'));
-    dibujarCard();
-}
+let tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+dibujarCard();
 
 // DEFINCIÓN DE FUNCIONES
-function saludar(){
-    saludo.innerHTML=`¡Hola,  ${nombre}!`;
+function saludar(usuario){
+    saludo.innerHTML=`¡Hola, ${usuario}!`;
 }
 
 function agregarTarea(idTarea, nombreTarea, fechaTarea, descripcionTarea, categoriaTarea){
@@ -46,8 +53,8 @@ function crearCard(tarea){
     // acciones tarea
     let accionesTarea=document.createElement("div");
     accionesTarea.className="acciones-tarea d-flex";
-    accionesTarea.innerHTML=`<i class="far fa-edit btn editar-${tarea.id}"></i>
-                        <button class="btn btn-eliminar-tarea" id="btn-eliminar-tarea" tarea-id="${tarea.id}">Hecho</button>`;
+    accionesTarea.innerHTML=`<i class="far fa-edit btn btn-editar-tarea" id="btn-editar-tarea" tarea-id="${tarea.id}"></i>
+                           <button class="btn btn-eliminar-tarea" id="btn-eliminar-tarea" tarea-id="${tarea.id}">Hecho</button>`;
 
     // card
     let card=document.createElement("div");
@@ -59,23 +66,34 @@ function crearCard(tarea){
     return card;
 }
 
+// dibuja la card de cada tarea y las guarda en el local storage
 function dibujarCard(){
     cardContainer.innerHTML="";
     tareas.forEach(
         (tarea) => {
             let card=crearCard(tarea);
             cardContainer.append(card);
-
+            
+            editarTarea();
             eliminarTarea();
         }
     )
     localStorage.setItem('tareas', JSON.stringify(tareas));
 }
 
-function eliminarTarea(){
-
+function editarTarea(){
     cardContainer.onclick = (e) => {
-        if(e.target.tagName == 'BUTTON'){
+        if(e.target.tagName.toLowerCase() === 'i'){
+          /*  const tareaId=parseInt(e.target.getAttribute('tarea-id'));
+            let tareaAEditar = tareas.find(tarea => tarea.id == tareaId); */
+            modal.classList.add('modal--show');
+        }
+    }
+}
+
+function eliminarTarea(){
+    cardContainer.onclick = (e) => {
+        if(e.target.tagName.toLowerCase() === 'button'){
             const tareaId=parseInt(e.target.getAttribute('tarea-id'));
             tareas = tareas.filter(tarea => tarea.id !== tareaId);
             dibujarCard();
@@ -97,14 +115,36 @@ class Tarea{
 
 // EVENTOS
 // Abrir y cerrar modal agregar tarea
-abrirModal.onclick = (e) => {
+abrirAggTarea.onclick = (e) => {
     e.preventDefault();
-    modal.classList.add('modal--show');
+    modalAggTarea.classList.add('modal--show');
 }
 
-cerrarModal.onclick = (e) => {
+cerrarAggTarea.onclick = (e) => {
     e.preventDefault();
-    modal.classList.remove('modal--show');
+    modalAggTarea.classList.remove('modal--show');
+}
+
+// Abrir y cerrar modal agregar categoría
+abrirAggCategoria.onclick = (e) => {
+    e.preventDefault();
+    modalAggCategoria.classList.add('modal--show');
+}
+
+cerrarAggCategoria.onclick = (e) => {
+    e.preventDefault();
+    modalAggCategoria.classList.remove('modal--show');
+}
+
+// Formulario bienvenida
+formBienvenida.onsubmit = (e) => {
+    e.preventDefault();
+    let formulario = e.target;
+    usuario = formulario.children[0].value;
+    saludar(usuario);
+    modalBienvenida.classList.remove('modal--show');
+
+    localStorage.setItem('user', usuario);
 }
 
 // Formulario agregar tarea
